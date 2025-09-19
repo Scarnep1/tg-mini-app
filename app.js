6f7b1b73029d3c1d07dee287f1658a87b6ab6f01
Scarnep1 <skarnep1@mail.ru>
2025-09-19 17:45:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
 const appsDatabase = {
-  'exchanges': [
-    { name: "Binance", url: "https://binance.com", icon: "images/i (2).webp" },
-    { name: "Bybit", url: "https://bybit.com", icon: "images/i (2).webp" }
-  ],
-  'channels': [
-    { name: "Мой канал", url: "https://t.me/chat_hamster_gamedev", icon: "images/i (3).webp" },
-    { name: "Крипто новости", url: "https://t.me/hamster_king_ru", icon: "images/i (2).webp" },
-    { name: "Трейдинг", url: "https://t.me/hamster_kombat_gamedev_heroes", icon: "images/i (2).webp" }
-  ]
+    'exchanges': [
+        { name: "Binance", url: "https://binance.com", icon: "images/i (2).webp" },
+        { name: "Bybit", url: "https://bybit.com", icon: "images/i (2).webp" }
+    ],
+    'channels': [
+        { name: "Moh kawan", url: "https://t.me/chat_hamster_gamedev", icon: "images/i (3).webp" },
+        { name: "Kpunro новости", url: "https://t.me/hamster_king_ru", icon: "images/i (2).webp" },
+        { name: "Tpekhyun", url: "https://t.me/hamster_kombat_gamedev_heroes", icon: "images/i (2).webp" }
+    ]
 };
 
 function showApps(sectionName) {
-  const appsGrid = document.getElementById('apps-grid');
-  appsGrid.innerHTML = '';
+    const appsGrid = document.getElementById('apps-grid');
+    appsGrid.innerHTML = "";
+    const appsToShow = appsDatabase[sectionName];
 
-  const appsToShow = appsDatabase[sectionName];
+    appsToShow.forEach(app => {
+        const appElement = document.createElement('div');
+        appElement.className = 'app-item';
+        appElement.innerHTML = 
+            <img src="${app.icon}" alt="${app.name}" class="app-avatar">
+            <span class="app-name">${app.name}</span>
+        ;
 
-  appsToShow.forEach(app => {
-    const appElement = document.createElement('div');
-    appElement.className = 'app-item';
-    appElement.innerHTML = 
-      <img src="${app.icon}" alt="${app.name}" class="app-avatar">
-      <span class="app-name">${app.name}</span>
-    ;
-
-    appElement.onclick = () => {
-      if (window.Telegram && Telegram.WebApp) {
-        Telegram.WebApp.openLink(app.url);
-      } else {
-        window.open(app.url, '_blank');
-      }
-    };
-
-    appsGrid.appendChild(appElement);
-  });
+        appElement.onclick = () => {
+            if (window.Telegram && Telegram.WebApp) {
+                Telegram.WebApp.openLink(app.url);
+            } else {
+                window.open(app.url, '_blank');
+            }
+        };
+        appsGrid.appendChild(appElement);
+    });
 }
 
 // Показываем раздел "Каналы" по умолчанию
 document.addEventListener('DOMContentLoaded', function() {
-  showApps('channels');
+    showApps('channels');
 });
